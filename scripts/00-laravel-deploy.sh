746569dabef334e8402aba45dcd4a9e046d378bc
Vlad Horpynych <19dynamo27@gmail.com>
2023-09-28 12:31:43 +0300

    Fixed deployment script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -12,6 +12,8 @@ ADD https://github.com/mlocati/docker-php-extension-installer/releases/latest/do
 RUN chmod +x /usr/local/bin/install-php-extensions && \
     install-php-extensions bcmath
 
+php -m
+php -v
 #echo "Running composer..."
 #cp /etc/secrets/.env .env
 ##composer global require hirak/prestissimo
