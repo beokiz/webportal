ded1b62a4758401fb8edc275d967e016f4239e9f
Vlad Horpynych <19dynamo27@gmail.com>
2023-09-28 12:15:20 +0300

    Fixed deployment script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,6 +4,8 @@
 # Copyright (c) 2023  Vlad Horpynych <19dynamo27@gmail.com>, Pavel Karpushevskiy <pkarpushevskiy@gmail.com>
 #
 
+cat /etc/os-release
+
 echo "Running composer..."
 cp /etc/secrets/.env .env
 #composer global require hirak/prestissimo
