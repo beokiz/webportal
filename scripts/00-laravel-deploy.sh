0e309a248bb750b779b3d4a070b55f9de1fc2084
Vlad Horpynych <19dynamo27@gmail.com>
2023-09-28 12:24:45 +0300

    Fixed deployment script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,22 +9,24 @@ cat /etc/os-release
 echo "Installing additional dependencies..."
 apk add php-bcmath
 
-echo "Running composer..."
-cp /etc/secrets/.env .env
-#composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
-
-echo "Clearing caches..."
-php artisan optimize:clear
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Running DB migrations & actions..."
-php artisan migrate --force
-php artisan actions --force
-
-echo "Done deploying"
+php -m
+php -v
+#echo "Running composer..."
+#cp /etc/secrets/.env .env
+##composer global require hirak/prestissimo
+#composer install --no-dev --working-dir=/var/www/html
+#
+#echo "Clearing caches..."
+#php artisan optimize:clear
+#
+#echo "Caching config..."
+#php artisan config:cache
+#
+#echo "Caching routes..."
+#php artisan route:cache
+#
+#echo "Running DB migrations & actions..."
+#php artisan migrate --force
+#php artisan actions --force
+#
+#echo "Done deploying"
