0e3ab24c2d2f5a7741dc37b2e1c99e5e8de0dccd
Robert Hoffmann <rob.hoffmann@posteo.de>
2024-12-04 17:58:28 +0100

    updated mail to test

--- app/Notifications/EmailVerifiedNotification.php
+++ app/Notifications/EmailVerifiedNotification.php
@@ -49,48 +49,88 @@ class EmailVerifiedNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        // Formatieren der Schulungsdaten
-        $trainingDetails = !empty($this->data) && is_array($this->data)
-            ? implode("\n", array_map(function ($training) {
-                return "• " . $training;
-            }, $this->data))
-            : "Noch keine Details verfügbar.";
+        // Ermitteln der ersten Kita des Benutzers
+        $kita = $notifiable->kitas->first();
 
-        $trainingDetailsFormatted = nl2br($trainingDetails); // Zeilenumbrüche zu <br/>
+        // Prüfen, ob es sich um eine zusammengelegte Schulung handelt
+        $isMergedTraining = $kita && $kita->num_pedagogical_staff <= 10;
 
-        // Loggen der bereitgestellten Daten
-        Log::info('Benachrichtigung: EmailVerifiedNotification', [
-            'notifiable' => $notifiable,
-            'data' => $this->data,
-            'training_details' => $trainingDetails,
+        // Schulungsdaten für zusammengelegte Schulungen
+        if ($isMergedTraining) {
+            $trainings = $notifiable->trainings;
+
+            // Logging der Trainingsdaten
+            if ($trainings->isNotEmpty()) {
+                Log::info('Trainings des Benutzers:', [
+                    'user_id' => $notifiable->id,
+                    'trainings' => $trainings->map->getNotificationsData(),
+                ]);
+            } else {
+                Log::warning('Keine Trainings für den Benutzer gefunden.', [
+                    'user_id' => $notifiable->id,
+                ]);
+            }
+
+            // Formatieren der Trainingsdetails
+            $trainingDetails = $trainings->isNotEmpty()
+                ? implode("\n", $trainings->map(function ($training) {
+                    $data = $training->getNotificationsData();
+                    Log::info('Training-Daten:', $data);
+                    return sprintf(
+                        "• %s\n  Datum 1: %s (%s)\n  Datum 2: %s (%s)\n  Ort: %s",
+                        $data['multiplier_name'] ?? 'Training',
+                        $data['first_date'],
+                        $data['first_date_start_and_end_time'],
+                        $data['second_date'],
+                        $data['second_date_start_and_end_time'],
+                        $data['location']
+                    );
+                })->toArray())
+                : "Noch keine Details verfügbar.";
+
+            $trainingDetailsFormatted = nl2br($trainingDetails);
+
+            Log::info('Formatiertes Trainings-Details:', [
+                'training_details' => $trainingDetailsFormatted,
+            ]);
+
+            return (new CustomMailMessage)
+                ->subject(__('notifications.email_verified.subject'))
+                ->greeting(__('notifications.email_verified.greeting', [
+                    'name' => $notifiable->full_name,
+                ]))
+                ->line(__('Ihre Schulung ist hiermit bestätigt.'))
+                ->line(__('Hier sind die Details:') . '<br/>' . $trainingDetailsFormatted)
+                ->line(__('Wir freuen uns auf Sie und Ihr Team.'))
+                ->salutation(__('notifications.email_verified.salutation'));
+        }
+
+        // Standard-Mail für andere Schulungen
+        if (!empty($this->data) && is_array($this->data)) {
+            $trainingProposals = count($this->data) > 1
+                ? "\n &#x2022; " . implode("\n &#x2022; ", (array) $this->data)
+                : "\n &#x2022; {$this->data[0]}";
+        } else {
+            $trainingProposals = "-";
+        }
+
+        $trainingProposals = nl2br($trainingProposals);
+
+        Log::info('Standard-Mail wird gesendet.', [
+            'user_id' => $notifiable->id,
+            'training_proposals' => $trainingProposals,
         ]);
 
-        // Erstellen der E-Mail
-        $mailMessage = (new CustomMailMessage)
+        return (new CustomMailMessage)
             ->subject(__('notifications.email_verified.subject'))
             ->greeting(__('notifications.email_verified.greeting', [
                 'name' => $notifiable->full_name,
             ]))
-            ->line(__('Ihre Schulung ist hiermit bestätigt.'))
-            ->line(__('Hier sind die Details:') . '<br/>' . $trainingDetailsFormatted)
-            ->line(__('Wir freuen uns auf Sie und Ihr Team.'))
+            ->line(__('notifications.email_verified.first_line', [
+                'training_proposals' => $trainingProposals,
+            ]))
+            ->line(__('notifications.email_verified.second_line'))
             ->salutation(__('notifications.email_verified.salutation'));
-
-        // Finalen E-Mail-Inhalt loggen
-        Log::info('Finaler E-Mail-Inhalt:', [
-            'subject' => __('notifications.email_verified.subject'),
-            'greeting' => __('notifications.email_verified.greeting', [
-                'name' => $notifiable->full_name,
-            ]),
-            'lines' => [
-                __('Ihre Schulung ist hiermit bestätigt.'),
-                __('Hier sind die Details:') . '<br/>' . $trainingDetailsFormatted,
-                __('Wir freuen uns auf Sie und Ihr Team.'),
-            ],
-            'salutation' => __('notifications.email_verified.salutation'),
-        ]);
-
-        return $mailMessage;
     }
 
     /**
